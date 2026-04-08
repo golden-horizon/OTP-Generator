6cfc2e20c665a30384068fea5287c6f0b3f45d53
Navid Ghobadpour <navid77g@gmail.com>
2026-04-08 17:20:59 +1000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { OTPGenerator } from "./OTPGenerator";
+
+export default function App() {
+  return (
+    <div className="app">
+      <OTPGenerator />
+    </div>
+  );
+}
